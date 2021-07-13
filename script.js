f9573ecf10be5442123a6b85f55ca4a1371860d3
deivson <dayvson04@hotmail.com>
2021-07-12 22:55:42 -0300

    adicionado o script js

---
+++ script.js
@@ -0,0 +1,54 @@
+//var colors = ["black", "white", "yellow", "green","blue"];
+
+//for (var i = 0; i < colors.length; i++)
+   //console.log(colors[i])
+
+
+   //for (var i = 1; i < 10; i++){
+       //console.log(i);
+  // }
+
+   //var i = 0;
+
+   //while(i <= 10) {
+       //console.log(i);
+        // i++;
+   //}
+    
+   //var i = 1;
+
+   //do {
+       //console.log(i);
+       //i++;
+   // } while (i < 10);
+   
+  // funcões javaescript
+
+  //function primeiro(name, lastName){
+      //console.log('holla' + name +  +lastName )
+  //} 
+
+  //primeiro("deivson", "araujo")
+
+
+  //classes em javascript
+
+  //class book{
+      //constructor(title, autthor,pages){
+          //this.title = title
+          //this.author = author
+          //this.title = pages
+      //}
+  
+    //read(){
+        //return 'estou lendo $'
+    //}
+  
+   // }
+
+
+   let test = function(){
+    const name;
+    let fullName = name + "" santos"";
+    console.log(fullName);
+    };
