4081683ed9259a96cebdf54331ba7f7fb49f7aa7
deivson <dayvson04@hotmail.com>
2021-07-16 22:08:57 -0300

    projeto atualizado

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@
      function addItem() {
       // Pegando os valores dos campo email
       const email = $('input[email=email]').val();
-      
+    
       // Criando uma string com objeto a ser adicioando np localstore
       const dataObj = JSON.stringify({email});
 
       localStorage.setItem('item', dataObj);
-     }
+      alert("email enviado com suceso") 
+    }
   
   
