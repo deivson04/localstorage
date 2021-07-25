502b0b071f6059161f721012dc93ab1e73790a7a
deivson <dayvson04@hotmail.com>
2021-07-25 00:23:58 -0300

    alteração no projeto

--- js/script.js
+++ js/script.js
@@ -3,11 +3,26 @@
       // Pegando os valores dos campo email
       const email = $('input[email=email]').val();
     
-      // Criando uma string com objeto a ser adicioando np localstore
+      // Criando uma string com objeto a ser adicioando n localstore
       const dataObj = JSON.stringify({email});
 
       localStorage.setItem('item', dataObj);
       alert("email enviado com suceso") 
+    }
+  
+
+     
+    function addIcar() {
+      // Pegando os valores dos campo produto
+      const no = $('input[nome=nome]').val();
+      const em = $('input[email=email]').val();
+      //const prod = $('input[produto=produto]').val();
+    
+      // Criando uma string com objeto a ser adicioando no localstore
+      const dataObj = JSON.stringify({no, em});
+
+      localStorage.setItem('produtos', dataObj);
+      alert("produto no localstore") 
     }
   
   
