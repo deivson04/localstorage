ad21318cac26e51bb79868d4d74755876f1ef44b
deivson <dayvson04@hotmail.com>
2021-07-25 02:52:09 -0300

    alterado o projeto

--- js/script.js
+++ js/script.js
@@ -6,23 +6,42 @@
       // Criando uma string com objeto a ser adicioando n localstore
       const dataObj = JSON.stringify({email});
 
-      localStorage.setItem('item', dataObj);
+      localStorage.setItem('Email', dataObj);
       alert("email enviado com suceso") 
+      
+    
     }
   
 
      
     function addIcar() {
       // Pegando os valores dos campo produto
-      const no = $('input[nome=nome]').val();
-      const em = $('input[email=email]').val();
-      //const prod = $('input[produto=produto]').val();
-    
+      const nome = $('input[nome=nome]').val();
+      const email = $('input[email=email]').val();
+      const placadevideo = $('input[placadevideo=placadevideo]').val();
+      const headerphone = $('input[headerphone=headerphone]').val();
+      const placamae = $('input[placamae=placamae]').val();
+      const endereco = $('input[endereco=endereco]').val();
+      
+      
       // Criando uma string com objeto a ser adicioando no localstore
-      const dataObj = JSON.stringify({no, em});
-
-      localStorage.setItem('produtos', dataObj);
+      const dataObj = JSON.stringify({nome, email, placadevideo, headerphone, placamae, endereco});
+      
+      localStorage.setItem('Informações', dataObj);
       alert("produto no localstore") 
+    }
+
+    function validar() {
+      // funcao validar
+      let email = document.getElementById("email");
+    
+      // verificar se o nome esta vazio
+      if(email.value == "") {
+        alert("Email não informado");
+
+        // Deixa o input com o focus
+        email.focus();
+      }
     }
   
   
